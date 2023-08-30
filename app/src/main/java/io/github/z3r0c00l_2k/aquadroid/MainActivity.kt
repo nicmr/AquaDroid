e8c3f006020ba7c44a1f4e2bc8f591dcf3f8f027
Nicolas Mohr <Nico.Mohr@gmx.net>
2023-08-30 19:01:15 +0200

    Make all buttons configurable

--- app/src/main/java/io/github/z3r0c00l_2k/aquadroid/MainActivity.kt
+++ app/src/main/java/io/github/z3r0c00l_2k/aquadroid/MainActivity.kt
@@ -3,11 +3,15 @@ package io.github.z3r0c00l_2k.aquadroid
 import android.app.NotificationManager
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.text.TextUtils
 import android.util.TypedValue
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.daimajia.androidanimations.library.Techniques
@@ -19,6 +23,13 @@ import io.github.z3r0c00l_2k.aquadroid.helpers.AlarmHelper
 import io.github.z3r0c00l_2k.aquadroid.helpers.SqliteHelper
 import io.github.z3r0c00l_2k.aquadroid.utils.AppUtils
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main_test.amount0Text
+import kotlinx.android.synthetic.main.activity_main_test.amount1Text
+import kotlinx.android.synthetic.main.activity_main_test.amount2Text
+import kotlinx.android.synthetic.main.activity_main_test.amount3Text
+import kotlinx.android.synthetic.main.activity_main_test.amount4Text
+import kotlinx.android.synthetic.main.activity_main_test.btnEdit
+import kotlinx.android.synthetic.main.activity_main_test.op300ml
 
 
 class MainActivity : AppCompatActivity() {
@@ -32,16 +43,23 @@ class MainActivity : AppCompatActivity() {
     private var selectedOption: Int? = null
     private var snackbar: Snackbar? = null
     private var doubleBackToExitPressedOnce = false
+    private var editMode: Boolean = false
 
+    // TODO: consider placing these in separate class to reduce array indexing required
+    private var configuredAmounts = ArrayList<Int>()
+    private var amountTextViews = ArrayList<TextView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_test)
 
         sharedPref = getSharedPreferences(AppUtils.USERS_SHARED_PREF, AppUtils.PRIVATE_MODE)
         sqliteHelper = SqliteHelper(this)
 
         totalIntake = sharedPref.getInt(AppUtils.TOTAL_INTAKE, 0)
+
+        setAmountTextViews()
+        setConfiguredAmountsDefault()
 
         if (sharedPref.getBoolean(AppUtils.FIRST_RUN_KEY, true)) {
             startActivity(Intent(this, WalkThroughActivity::class.java))
@@ -52,15 +70,78 @@ class MainActivity : AppCompatActivity() {
         }
 
         dateNow = AppUtils.getCurrentDate()!!
+    }
 
+    private fun setConfiguredAmountsDefault() {
+        this.configuredAmounts.addAll(listOf(50, 100, 150, 200, 250, 300))
+    }
+    private fun setAmountTextViews() {
+        this.amountTextViews.addAll(
+            listOf(
+                amount0Text,
+                amount1Text,
+                amount2Text,
+                amount3Text,
+                amount4Text,
+                amount5Text
+            ))
     }
 
     fun updateValues() {
         totalIntake = sharedPref.getInt(AppUtils.TOTAL_INTAKE, 0)
-
         inTook = sqliteHelper.getIntook(dateNow)
-
         setWaterLevel(inTook, totalIntake)
+    }
+
+    fun addIntake(view: View, milliliters: Int) {
+        // TODO: find out what this check does
+        if (sqliteHelper.addIntook(dateNow, milliliters) > 0) {
+            inTook += milliliters
+            setWaterLevel(inTook, totalIntake)
+            Snackbar.make(view, "Your water intake was saved", Snackbar.LENGTH_SHORT).show()
+        }
+        if (inTook >= totalIntake) {
+            val mNotificationManager : NotificationManager =
+                getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            mNotificationManager.cancelAll()
+        }
+    }
+
+    fun intakeAmountEditDialogue(view: View, fieldIndex: Int) {
+//        opCustom.setOnClickListener {
+//
+//            op50ml.background = getDrawable(outValue.resourceId)
+//            op100ml.background = getDrawable(outValue.resourceId)
+//            op150ml.background = getDrawable(outValue.resourceId)
+//            op200ml.background = getDrawable(outValue.resourceId)
+//            op250ml.background = getDrawable(outValue.resourceId)
+//            opCustom.background = getDrawable(R.drawable.option_select_bg)
+//
+//        }
+        if (snackbar != null) {
+            snackbar?.dismiss()
+        }
+        var li = LayoutInflater.from(this)
+        val dialogView = li.inflate(R.layout.custom_input_dialog, null)
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setView(dialogView)
+
+        val userInput = dialogView.findViewById(R.id.etCustomInput) as TextInputLayout
+
+        alertDialogBuilder.setPositiveButton("OK") { dialog, id ->
+            val inputText = userInput.editText!!.text.toString() // check if there is an alternative to !! here
+            if (!TextUtils.isEmpty(inputText)) {
+//                tvCustom.text = "${inputText} ml"
+                this.amountTextViews[fieldIndex].text = "${inputText} ml"
+                configuredAmounts[fieldIndex] = inputText.toInt()
+            }
+        }.setNegativeButton("Cancel") { dialog, _ ->
+            dialog.cancel()
+        }
+
+        var alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+
     }
 
     override fun onStart() {
@@ -98,38 +179,48 @@ class MainActivity : AppCompatActivity() {
             bottomSheetFragment.show(supportFragmentManager, bottomSheetFragment.tag)
         }
 
-        fabAdd.setOnClickListener {
-            if (selectedOption != null) {
-                if ((inTook * 100 / totalIntake) <= 140) {
-                    if (sqliteHelper.addIntook(dateNow, selectedOption!!) > 0) {
-                        inTook += selectedOption!!
-                        setWaterLevel(inTook, totalIntake)
+//        fabAdd.setOnClickListener {
+//            if (selectedOption != null) {
+//                if ((inTook * 100 / totalIntake) <= 140) {
+//                    if (sqliteHelper.addIntook(dateNow, selectedOption!!) > 0) {
+//                        inTook += selectedOption!!
+//                        setWaterLevel(inTook, totalIntake)
+//
+//                        Snackbar.make(it, "Your water intake was saved...!!", Snackbar.LENGTH_SHORT)
+//                            .show()
+//
+//                    }
+//                } else {
+//                    Snackbar.make(it, "You already achieved the goal", Snackbar.LENGTH_SHORT).show()
+//                }
+//                // Change: remove selection reset on Add
+////                selectedOption = null
+////                tvCustom.text = "Custom"
+////                op50ml.background = getDrawable(outValue.resourceId)
+////                op100ml.background = getDrawable(outValue.resourceId)
+////                op150ml.background = getDrawable(outValue.resourceId)
+////                op200ml.background = getDrawable(outValue.resourceId)
+////                op250ml.background = getDrawable(outValue.resourceId)
+////                opCustom.background = getDrawable(outValue.resourceId)
+//
+//                // remove pending notifications
+//                val mNotificationManager : NotificationManager =
+//                    getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+//                mNotificationManager.cancelAll()
+//            } else {
+//                YoYo.with(Techniques.Shake)
+//                    .duration(700)
+//                    .playOn(cardView)
+//                Snackbar.make(it, "Please select an option", Snackbar.LENGTH_SHORT).show()
+//            }
+//        }
 
-                        Snackbar.make(it, "Your water intake was saved...!!", Snackbar.LENGTH_SHORT)
-                            .show()
-
-                    }
-                } else {
-                    Snackbar.make(it, "You already achieved the goal", Snackbar.LENGTH_SHORT).show()
-                }
-                selectedOption = null
-                tvCustom.text = "Custom"
-                op50ml.background = getDrawable(outValue.resourceId)
-                op100ml.background = getDrawable(outValue.resourceId)
-                op150ml.background = getDrawable(outValue.resourceId)
-                op200ml.background = getDrawable(outValue.resourceId)
-                op250ml.background = getDrawable(outValue.resourceId)
-                opCustom.background = getDrawable(outValue.resourceId)
-
-                // remove pending notifications
-                val mNotificationManager : NotificationManager =
-                    getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-                mNotificationManager.cancelAll()
+        btnEdit.setOnClickListener {
+            editMode = !editMode
+            if (editMode) {
+                btnEdit.backgroundTintList = ColorStateList.valueOf(Color.BLUE)
             } else {
-                YoYo.with(Techniques.Shake)
-                    .duration(700)
-                    .playOn(cardView)
-                Snackbar.make(it, "Please select an option", Snackbar.LENGTH_SHORT).show()
+                btnEdit.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#41B279"))
             }
         }
 
@@ -159,13 +250,19 @@ class MainActivity : AppCompatActivity() {
             if (snackbar != null) {
                 snackbar?.dismiss()
             }
-            selectedOption = 50
-            op50ml.background = getDrawable(R.drawable.option_select_bg)
-            op100ml.background = getDrawable(outValue.resourceId)
-            op150ml.background = getDrawable(outValue.resourceId)
-            op200ml.background = getDrawable(outValue.resourceId)
-            op250ml.background = getDrawable(outValue.resourceId)
-            opCustom.background = getDrawable(outValue.resourceId)
+            if (editMode) {
+                intakeAmountEditDialogue(it, 0)
+
+            } else {
+                addIntake(it, configuredAmounts[0])
+            }
+//            selectedOption = 50
+//            op50ml.background = getDrawable(R.drawable.option_select_bg)
+//            op100ml.background = getDrawable(outValue.resourceId)
+//            op150ml.background = getDrawable(outValue.resourceId)
+//            op200ml.background = getDrawable(outValue.resourceId)
+//            op250ml.background = getDrawable(outValue.resourceId)
+//            opCustom.background = getDrawable(outValue.resourceId)
 
         }
 
@@ -173,94 +270,56 @@ class MainActivity : AppCompatActivity() {
             if (snackbar != null) {
                 snackbar?.dismiss()
             }
-            selectedOption = 100
-            op50ml.background = getDrawable(outValue.resourceId)
-            op100ml.background = getDrawable(R.drawable.option_select_bg)
-            op150ml.background = getDrawable(outValue.resourceId)
-            op200ml.background = getDrawable(outValue.resourceId)
-            op250ml.background = getDrawable(outValue.resourceId)
-            opCustom.background = getDrawable(outValue.resourceId)
-
+            if (editMode) {
+                intakeAmountEditDialogue(it, 1)
+            } else {
+                addIntake(it, configuredAmounts[1])
+            }
         }
 
         op150ml.setOnClickListener {
             if (snackbar != null) {
                 snackbar?.dismiss()
             }
-            selectedOption = 150
-            op50ml.background = getDrawable(outValue.resourceId)
-            op100ml.background = getDrawable(outValue.resourceId)
-            op150ml.background = getDrawable(R.drawable.option_select_bg)
-            op200ml.background = getDrawable(outValue.resourceId)
-            op250ml.background = getDrawable(outValue.resourceId)
-            opCustom.background = getDrawable(outValue.resourceId)
-
+            if (editMode) {
+                intakeAmountEditDialogue(it, 2)
+            } else {
+                addIntake(it, configuredAmounts[2])
+            }
         }
 
         op200ml.setOnClickListener {
             if (snackbar != null) {
                 snackbar?.dismiss()
             }
-            selectedOption = 200
-            op50ml.background = getDrawable(outValue.resourceId)
-            op100ml.background = getDrawable(outValue.resourceId)
-            op150ml.background = getDrawable(outValue.resourceId)
-            op200ml.background = getDrawable(R.drawable.option_select_bg)
-            op250ml.background = getDrawable(outValue.resourceId)
-            opCustom.background = getDrawable(outValue.resourceId)
-
+            if (editMode) {
+                intakeAmountEditDialogue(it, 3)
+            } else {
+                addIntake(it, configuredAmounts[3])
+            }
         }
 
         op250ml.setOnClickListener {
             if (snackbar != null) {
                 snackbar?.dismiss()
             }
-            selectedOption = 250
-            op50ml.background = getDrawable(outValue.resourceId)
-            op100ml.background = getDrawable(outValue.resourceId)
-            op150ml.background = getDrawable(outValue.resourceId)
-            op200ml.background = getDrawable(outValue.resourceId)
-            op250ml.background = getDrawable(R.drawable.option_select_bg)
-            opCustom.background = getDrawable(outValue.resourceId)
-
+            if (editMode) {
+                intakeAmountEditDialogue(it, 4)
+            } else {
+                addIntake(it, configuredAmounts[4])
+            }
         }
 
-        opCustom.setOnClickListener {
+        op300ml.setOnClickListener {
             if (snackbar != null) {
                 snackbar?.dismiss()
             }
-
-            val li = LayoutInflater.from(this)
-            val promptsView = li.inflate(R.layout.custom_input_dialog, null)
-
-            val alertDialogBuilder = AlertDialog.Builder(this)
-            alertDialogBuilder.setView(promptsView)
-
-            val userInput = promptsView
-                .findViewById(R.id.etCustomInput) as TextInputLayout
-
-            alertDialogBuilder.setPositiveButton("OK") { dialog, id ->
-                val inputText = userInput.editText!!.text.toString()
-                if (!TextUtils.isEmpty(inputText)) {
-                    tvCustom.text = "${inputText} ml"
-                    selectedOption = inputText.toInt()
-                }
-            }.setNegativeButton("Cancel") { dialog, id ->
-                dialog.cancel()
+            if (editMode) {
+                intakeAmountEditDialogue(it, 5)
+            } else {
+                addIntake(it, configuredAmounts[5])
             }
-
-            val alertDialog = alertDialogBuilder.create()
-            alertDialog.show()
-
-            op50ml.background = getDrawable(outValue.resourceId)
-            op100ml.background = getDrawable(outValue.resourceId)
-            op150ml.background = getDrawable(outValue.resourceId)
-            op200ml.background = getDrawable(outValue.resourceId)
-            op250ml.background = getDrawable(outValue.resourceId)
-            opCustom.background = getDrawable(R.drawable.option_select_bg)
-
         }
-
     }
 
 
